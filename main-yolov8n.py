d115eb1e9dae66309ad8f6d8018bc5d683a6495a
sheminrad <samiei.shemin@gmail.com>
2026-03-26 00:01:54 +0200

    second run, train and val fixed on China_Drone

--- main-yolov8n.py
+++ main-yolov8n.py
@@ -1,7 +1,25 @@
 from ultralytics import YOLO
 
-# Load the custom YAML architecture
-model = YOLO("lee-yolov8n.yaml")
 
-# Train the model (using a dummy or real dataset)
-results = model.train(data="coco8.yaml", epochs=1, imgsz=640)
+def main():
+    print("1. Initializing Custom LEE-YOLO Architecture...")
+    model = YOLO("lee-yolov8n.yaml")
+
+    print("2. Starting Training on China_Drone Dataset with Nvidia RTX...")
+    results = model.train(
+        data="china_drone.yaml",
+        epochs=300,
+        batch=16,  # <-- FIX 1: Drop batch size from 32 to 16
+        workers=2,  # <-- FIX 2: Drop workers from 8 to 2 (This saves MASSIVE System RAM)
+        imgsz=640,
+        device=0,
+        optimizer="AdamW",
+        name="lee_yolo_rdd2022",
+        deterministic=False
+    )
+
+    print("✅ Training Complete!")
+
+
+if __name__ == '__main__':
+    main()
